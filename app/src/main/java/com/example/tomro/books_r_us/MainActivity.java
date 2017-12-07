ec0f0aeb5c530228641a315378739bee4b0215ad
ecstaticrainbow <tjbarham@btinternet.com>
2017-12-07 17:22:03 +0000

    Basic Navigation Implemented

--- app/src/main/java/com/example/tomro/books_r_us/MainActivity.java
+++ app/src/main/java/com/example/tomro/books_r_us/MainActivity.java
@@ -1,13 +1,102 @@
 package com.example.tomro.books_r_us;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity implements homeFragment.OnFragmentInteractionListener, booksFragment.OnFragmentInteractionListener, searchFragment.OnFragmentInteractionListener {
+
+    ViewPager mViewPager;
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.pager);
+
+        SimpleFragmentPagerAdapter fragmentPagerAdapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
+        mViewPager.setAdapter(fragmentPagerAdapter);
+
+
+
+
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mViewPager.addOnPageChangeListener(mOnPageChangeListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mViewPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_books:
+                    mViewPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_search:
+                    mViewPager.setCurrentItem(2);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        // This method will be invoked when the current page is scrolled
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        // This method will be invoked when a new page becomes selected.
+        @Override
+        public void onPageSelected(int position) {
+
+            switch (position) {
+                case 0:
+                    navigation.setSelectedItemId(R.id.navigation_home);
+                case 1:
+                    navigation.setSelectedItemId(R.id.navigation_books);
+                case 2:
+                    navigation.setSelectedItemId(R.id.navigation_search);
+            }
+
+        }
+
+        // Called when the scroll state changes:
+        // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/tomro/books_r_us/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.tomro.books_r_us;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by tomro on 06/12/2017.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    // This determines the fragment for each tab
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return homeFragment.newInstance("home","test");
+        } else if (position == 1){
+            return booksFragment.newInstance("books","test");
+        } else if (position == 2){
+            return searchFragment.newInstance("search","test");
+        } else {
+            return homeFragment.newInstance("home","test");
+        }
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page " + position;
+    }
+
+    // This determines the number of tabs
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+}
